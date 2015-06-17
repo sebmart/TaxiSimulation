76615577fc053126b2af405f1242ab19b8de383b
Sebastien Martin <92sebastien@gmail.com>
2015-06-16 22:49:39 -0400

    added test file andcorrected typo

--- src/cities/metropolis.jl
+++ src/cities/metropolis.jl
@@ -225,7 +225,7 @@ function generateCustomers!(sim::Metropolis, demand::Float64)
         dest = coordToLoc(rand(1:sim.width), rand(1:sim.width), 0, sim)
         orig = coordToLoc(rand(1:sim.subWidth), rand(1:sim.subWidth), rand(1:sim.nSub), sim)
       end
-      pathTime = city.sp.traveltime[orig,dest]
+      pathTime = sim.sp.traveltime[orig,dest]
 
       if pathTime + i <= sim.nTime
         price = (hourFare(tCurrent)/120)*pathTime
