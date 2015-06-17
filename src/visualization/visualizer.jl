8ce12b9ca7cbd65c41419d3852d35f22340284de
Brandon <bzeng@mit.edu>
2015-06-17 16:20:38 -0400

    Visualizer for simple usage

--- src/visualization/visualizer.jl
+++ src/visualization/visualizer.jl
@@ -4,3 +4,14 @@ using HDF5
 using JLD
 using SFML
 using LightGraphs
+
+cd("/Users/bzeng/Dropbox (MIT)/7\ Coding/UROP/taxi-simulation/src/visualization");
+include("setup.jl")
+
+width, nTime, nTaxis, nCusts = 10, 100, 10, 40
+city = SquareCity(width);
+generateProblem!(city, nTaxis, nTime, nCusts)
+presol = insertionsDescent(city, 500);
+sol = TaxiSolution(city, presol);
+
+visualize(city, sol)
