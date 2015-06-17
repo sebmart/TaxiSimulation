abb5631a1d31a062f3ab5885ab76f07c1a6bdc69
Brandon <bzeng@mit.edu>
2015-06-17 16:51:16 -0400

    Added comments

--- src/visualization/visualizer.jl
+++ src/visualization/visualizer.jl
@@ -5,13 +5,18 @@ using JLD
 using SFML
 using LightGraphs
 
+# Loads the neccessary functions and scripts to create the visualization
 cd("/Users/bzeng/Dropbox (MIT)/7\ Coding/UROP/taxi-simulation/src/visualization");
 include("visualizerSetup.jl")
 
+# Modify following variables to create city of your choosing
 width, nTime, nTaxis, nCusts = 10, 100, 10, 40
 city = SquareCity(width);
 generateProblem!(city, nTaxis, nTime, nCusts)
+
+# Generates solution to the above problem
 presol = insertionsDescent(city, 500);
 sol = TaxiSolution(city, presol);
 
+# Creates a visualization of the taxi problem and solution
 visualize(city, sol)
