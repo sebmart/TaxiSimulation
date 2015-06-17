56e41cb7ca5847058e87df3595ab2e8b6cdda227
Sebastien Martin <92sebastien@gmail.com>
2015-06-16 20:21:21 -0400

    typo

--- test/squareCity.jl
+++ test/squareCity.jl
@@ -38,6 +38,6 @@ sol1  = intervalOpt(city, sol2);
 testSolution(city, sol1)
 testSolution(city, sol2)
 testSolution(city, sol3)
-testSolution(city, expandWindows(city,sol4))
+testSolution(city, expandWindows!(city,sol4))
 
 testSolution(city, IntervalSolution(city, TaxiSolution(city, sol1)))
