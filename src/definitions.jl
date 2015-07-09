e04b58d70d7b4aec0b57a2e2a1b966be4f57bfe5
Sebastien Martin <92sebastien@gmail.com>
2015-07-09 11:20:54 -0400

    indent bis

--- src/definitions.jl
+++ src/definitions.jl
@@ -10,49 +10,49 @@ typealias Road      Edge
 
 "All data needed to represent a customer"
 immutable Customer
-  "customer id"
-  id::Int
-  "Pick-up node in the graph"
-  orig::Int
-  "drop-off node in the graph"
-  dest::Int
-  "time of call for online simulations"
-  tcall::Float64
-  "Earliest time for pickup"
-  tmin::Float64
-  "Latest time for pickup"
-  tmaxt::Float64
-  "Fare paid by customer for the ride"
-  price::Float64
+    "customer id"
+    id::Int
+    "Pick-up node in the graph"
+    orig::Int
+    "drop-off node in the graph"
+    dest::Int
+    "time of call for online simulations"
+    tcall::Float64
+    "Earliest time for pickup"
+    tmin::Float64
+    "Latest time for pickup"
+    tmaxt::Float64
+    "Fare paid by customer for the ride"
+    price::Float64
 end
 
 "All data needed to represent a taxi"
 immutable Taxi
-  id::Int
-  initPos::Int
+    id::Int
+    initPos::Int
 end
 
 """
 TaxiProblem: All data needed for simulation
-  has to include:
-    network::Network (graph of city)
-    roadTime::SparseMatrixCSC{Int,Int} Time to cross a road
-    roadCost::SparseMatrixCSC{Float64,Int} Cost to cross a road
-    paths::Path Contain paths information
-    custs::Array{Customer,1} (customers)
-    taxis::Array{Taxi,1} (taxis)
-    nTime::Float64 last possible pickup time
-    waitingCost::Float64 cost of waiting
-    discreteTime::Bool if time is discrete
+has to include:
+network::Network (graph of city)
+roadTime::SparseMatrixCSC{Int,Int} Time to cross a road
+roadCost::SparseMatrixCSC{Float64,Int} Cost to cross a road
+paths::Path Contain paths information
+custs::Array{Customer,1} (customers)
+taxis::Array{Taxi,1} (taxis)
+nTime::Float64 last possible pickup time
+waitingCost::Float64 cost of waiting
+discreteTime::Bool if time is discrete
 """
 abstract TaxiProblem
 
 "Represent the assignement of a customer to a taxi"
 immutable CustomerAssignment
-  "id of taxi, 0 if customer is not assigned"
-  id::Int
-  timeIn::Float64
-  timeOut::Float64
+    "id of taxi, 0 if customer is not assigned"
+    id::Int
+    timeIn::Float64
+    timeOut::Float64
 end
 
 """
@@ -60,17 +60,17 @@ Represent the actions of a taxi during a simulation:
 his path and interactions with customers
 """
 type TaxiActions
-  "roads in order : (time, road)"
-  path::Vector{ Tuple{ Float64, Road}}
-  "customer in order: (id, pickup, dropoff)"
-  custs::Vector{ CustomerAssignment} #
+    "roads in order : (time, road)"
+    path::Vector{ Tuple{ Float64, Road}}
+    "customer in order: (id, pickup, dropoff)"
+    custs::Vector{ CustomerAssignment} #
 end
 
 "Represent the solution of a simulation"
 type TaxiSolution
-  taxis::Array{TaxiActions, 1}
-  notTaken::BitVector
-  cost::Float64
+    taxis::Array{TaxiActions, 1}
+    notTaken::BitVector
+    cost::Float64
 end
 
 """
@@ -80,59 +80,59 @@ abstract Paths
 
 "Paths that are the fastest in time"
 type ShortestPaths <: Paths
-  traveltime::Array{Float64,2}
-  travelcost::Array{Float64,2}
-  previous::Array{Int,2}
+    traveltime::Array{Float64,2}
+    travelcost::Array{Float64,2}
+    previous::Array{Int,2}
 end
 
 "Paths with an extra cost for turning left"
 type RealPaths <: Paths
-  traveltime::Array{Float64,2}
-  travelcost::Array{Float64,2}
-  newRoadTime::AbstractArray{Float64,2}
-  newRoadCost::AbstractArray{Float64,2}
-  newPrevious::Array{Int,2}
-  newDest::Array{Int,2}
-  nodeMapping::Vector{Int}
+    traveltime::Array{Float64,2}
+    travelcost::Array{Float64,2}
+    newRoadTime::AbstractArray{Float64,2}
+    newRoadCost::AbstractArray{Float64,2}
+    newPrevious::Array{Int,2}
+    newDest::Array{Int,2}
+    nodeMapping::Vector{Int}
 end
 
 "Dijkstra Heap entry"
 immutable DijkstraEntry{Float64}
-  vertex::Int
-  dist::Float64
-  cost::Float64
+    vertex::Int
+    dist::Float64
+    cost::Float64
 end
 
 "Represent the pickup time window of a customer"
 type AssignedCustomer
-  id::Int
-  tInf::Float64
-  tSup::Float64
+    id::Int
+    tInf::Float64
+    tSup::Float64
 end
 
 "represent a time-window solution (only work with fixed timings)"
 type IntervalSolution
-  custs::Vector{Vector{AssignedCustomer}}
-  notTaken::BitVector
-  cost::Float64
+    custs::Vector{Vector{AssignedCustomer}}
+    notTaken::BitVector
+    cost::Float64
 end
 
 "x and y coordinates, to represent ENU positions"
 immutable Coordinates
-  x::Float64
-  y::Float64
+    x::Float64
+    y::Float64
 end
 
 
 """
-  Type used to solve online simulation problems
-  Needs to implement initialize!(om::OnlineMethod, pb::TaxiProblem), update!(om::OnlineMethod,
+Type used to solve online simulation problems
+Needs to implement initialize!(om::OnlineMethod, pb::TaxiProblem), update!(om::OnlineMethod,
     newEndTime::Float64, newCustomers::Vector{Customer})
-  initialize! initializes a given OnlineMethod with a selected taxi problem without customers
-  update! updates OnlineMethod to account for new customers, returns a list of TaxiActions
-  since the last update
-"""
-abstract OnlineMethod
+    initialize! initializes a given OnlineMethod with a selected taxi problem without customers
+    update! updates OnlineMethod to account for new customers, returns a list of TaxiActions
+    since the last update
+    """
+    abstract OnlineMethod
 
-#time epsilon for float comparisons
-EPS = 1e-5
+    #time epsilon for float comparisons
+    EPS = 1e-5
