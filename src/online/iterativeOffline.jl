c773518185f8d6b006c67eefb8b9872286aaa511
Brandon <bzeng@mit.edu>
2015-07-13 10:38:38 -0400

    Updated online method

--- src/online/iterativeOffline.jl
+++ src/online/iterativeOffline.jl
@@ -1,118 +1,103 @@
 type IterativeOffline <: OnlineMethod
 	solver::Function
 	tHorizon::Float64
-	endTime::Float64
+	startTime::Float64
 	
 	pb::TaxiProblem
-	# currentSolution::TaxiSolution
-	# currentCustomers::Vector{Customer}
-	# currentTaxis::Vector{Taxi}
-	# endTime::Float64
-	# taxiActions::Vector{TaxiActions}
-	# solution::IntervalSolution
 
-	function IterativeOffline(solver::Function, tHorizon::Float64)
+	customers::Vector{Customer}
+	notTaken::Vector{Bool}
+
+	function IterativeOffline(tHorizon::Float64)
 		offline = new()
-		offline.solver =  solver
 		offline.tHorizon = tHorizon
+		offline.startTime = 0.0
+		offline.customers = Customer[]
+		offline.notTaken = Bool[]
 		return offline
 	end
 end
 
-# Should store endtime as well
-# Change to initialize with no customers at all
-
-# Initializes a given OnlineMethod with a selected taxi problem without customers
-function initialize!(om::OnlineMethod, pb::TaxiProblem, p::Float64)
+"""
+Initializes a given OnlineMethod with a selected taxi problem without customers
+"""
+function initialize!(om::OnlineMethod, pb::TaxiProblem)
 	reducedPb = copy(pb)
-	reducedPb.custs = Vector{Customer}
+	reducedPb.custs = Customer[]
 	om.pb = reducedPb
-	om.period = p
 end
+
 """
 Updates OnlineMethod to account for new customers, returns a list of TaxiActions 
 since the last update. Needs initial information to start from. 
 """
 # remove/ignore customers that are after THorizon?
-function update!(om::OnlineMethod, step::Int64, newCustomers::Vector{Customer})
-	append!(om.pb.custs, newCustomers)	
+function update!(om::OnlineMethod, endTime::Float64, newCustomers::Vector{Customer})
+	# Sets the time window for the offline solver	
+	startOffline = om.startTime
+	finishOffline = startOffline + om.tHorizon
+	# shift = endTime - start
 	
-	# Make sure customers are picked up before THorizon
+	# Adds the new customers to the problem's customers
+	append!(om.customers, newCustomers)	
+	append!(om.notTaken, [true for i in length(om.customers)])
+	sort!(om.customers, x->x.tmin)
 
-	offlineSolution = TaxiSolution(om.pb, om.solver(om.pb, 100))
-
-	# Not taken customers should be carried over, modifying tmaxt or tmin as necessary
-
-	# Map Customer IDs to current indices in om.pb.custs as ID != index here
-	IDtoIndex = Dict{Int64, Int64}
-	for i = 1:length(newCustomers)
-		IDtoIndex[newCustomers[i].id] = i
-	end
-
-	for (k, TaxiAction) in enumerate(offlineSolution.taxis)
-		if TaxiAction.path[1][1] > step * om.period
-			TaxiAction.path = Vector{Tuple{Float64, Road}}
-			for assignment in TaxiAction.custs
-				# Fix indexing with created dictionary
-				offline.notTaken[IDtoIndex[assignment.id]] = true
+	# Identifies current customers with pickup window within the time window
+	currentCustomers = Customer[]
+	IDtoIndex = Int[]
+	for customer in om.customers
+		if om.notTaken[customer.id]
+			if customer.tmin < startOffline 
+				if customer.tmaxt >= startOffline 
+					tmin = 0.0; tmaxt = min(customer.tmaxt, finishOffline) - startOffline 
+					push!(IDtoIndex, customer.id)
+					c = Customer(length(IDtoIndex), customer.orig, customer.dest, 0, tmin, tmaxt, customer.price)
+					push!(currentCustomers, c)
+				end
+			elseif customer.tmin < finish
+			 	tmaxt = min(customer.tmaxt, finishOffline) - startOffline
+			 	push!(IDtoIndex, customer.id)
+				c = Customer(length(IDtoIndex), customer.orig, customer.dest, 0, customer.tmin - startOffline, tmaxt, customer.price)
+				push!(currentCustomers, c)
 			end
-			TaxiAction.custs = Vector{CustomerAssignment}
 		end
-		om.pb.Taxis[k].initPos = om.pb.custs[TaxiAction.custs[end].id].dest
-		if TaxiAction.custs[end].timeOut <= step * period
-			om.pb.Taxis[k].initTime = 0
-		else
-			om.pb.Taxis[k].initTime = TaxiAction.custs[end].timeOut - om.period
+	end
+
+	# Sets the problem's customers to those identified within the time window, and solves
+	om.pb.custs = currentCustomers
+	om.pb.nTime = om.tHorizon
+	offlineSolution = TaxiSolution(om.pb,localDescent(om.pb, 100))
+
+	onlineTaxiActions = TaxiActions[TaxiActions(Tuple{ Float64, Road}[], CustomerAssignment[]) for i in 1:length(om.pb.taxis)]
+	# Not taken customers should be carried over, modifying tmaxt or tmin as necessary
+	for (k, TaxiAction) in enumerate(offlineSolution.taxis)
+		for (i, customer) in enumerate(TaxiAction.custs)
+			if TaxiAction.custs[i].timeIn + startOffline > endTime
+				break
+			else
+				om.notTaken[IDtoIndex[customer.id]] = false
+				c = CustomerAssignment(IDtoIndex[customer.id], customer.timeIn + startOffline, customer.timeOut + startOffline)
+				push!(onlineTaxiActions[k].custs, c)
+			end
 		end
+		for (t, road) in TaxiAction.path
+			if t + startOffline >= onlineTaxiActions[k].custs[end].timeOut - EPS
+				break
+			else
+				push!(onlineTaxiActions[k].path, (t + startOffline, road))
+			end
+		end
+		(t, road) = onlineTaxiActions[k].path[end]
+		newt = max(t + om.pb.roadTime[src(road), dst(road)] - startOffline, 0.0)
+		om.pb.taxis[k] = Taxi(om.pb.taxi[k].id, dst(road), newt)
 	end
 	
-	# Check for tmin, tmaxt for each customer. No reason to add customer to next horizon
-	# if they're no longer able to be picked up
-	remainingCusts = Vector{Customer}
-	for i = 1:length(offlineSolution.notTaken)
-		if offlineSolution.notTaken[i]
-			# Filter out desired customers that fit the time bill
-			push!(remainingCusts, om.pb.custs[i])
-		end
-	end
+	om.startTime = endTime
 
-	om.pb.custs = remainingCusts
-
-	for TaxiAction in offlineSolution.taxis
-		for path in TaxiAction.path
-			path[1] += (step - 1) * om.period
-		end
-		for assignment in TaxiAction.custs
-			assignment.timeIn += (step - 1) * om.period
-			assignment.timeOut += (step - 1) * om.period
-		end
-	end
-
-	return offlineSolution.taxis
+	# Returns new TaxiActions to OnlineSimulation
+	return onlineTaxiActions
 end
 
-onlineSimulation(pb, IterativeOffline(localDescent, 30))
+onlineSimulation(pb, IterativeOffline(30))
 
-function localDescent(pb::TaxiProblem, maxTry::Int, start::IntervalSolution = orderedInsertions(pb))
-    nTaxis = length(pb.taxis)
-    println("Start, $(-start.cost) dollars")
-    sol =  copySolution(start)
-    best = sol.cost
-    for trys in 1:maxTry
-        k = rand(1:nTaxis)
-        k2 = rand( 1 :(nTaxis-1))
-        k2 =  k2 >= k ? k2+1 : k2
-        if isempty(sol.custs[k])
-            continue
-        end
-        i = rand(1:length(sol.custs[k]))
-        sol = splitAndMove!(pb, sol, k, i, k2)
-        if sol.cost < best
-            print("\r====Try: $(trys), $(-sol.cost) dollars                  ")
-            best = sol.cost
-        end
-    end
-    expandWindows!(pb, sol)
-    print("\r====Final: $(-sol.cost) dollars              \n")
-    return sol
-end
