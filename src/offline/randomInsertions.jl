bfd1e72c444c4413a62c39d452f885d88e2acb9c
Sebastien Martin <semartin@mit.edu>
2015-11-30 16:37:56 -0500

    verbose parameter

--- src/offline/randomInsertions.jl
+++ src/offline/randomInsertions.jl
@@ -2,27 +2,27 @@
 #-- Try random orders, keep the best one
 #----------------------------------------
 include("orderedInsertions.jl")
-function randomInsertions(pb::TaxiProblem, n::Int; benchmark=false)
+function randomInsertions(pb::TaxiProblem, n::Int; benchmark=false, verbose=true)
 
     initT = time()
 
 
     order = randomOrder(pb)
     best = orderedInsertions(pb, order)
-    println("Try: 1, $(-best.cost) dollars")
+    verbose && println("Try: 1, $(-best.cost) dollars")
     benchmark && (benchData = BenchmarkPoint[BenchmarkPoint(time()-initT,-best.cost,Inf)])
     for trys in 2:n
         sol = orderedInsertions(pb, order)
 
         if sol.cost < best.cost
-            print("\r====Try: $(trys), $(-sol.cost) dollars                  ")
+            verbose && print("\r====Try: $(trys), $(-sol.cost) dollars                  ")
             benchmark && push!(benchData, BenchmarkPoint(time()-initT,-sol.cost,Inf))
             best = sol
         end
 
         order = randomOrder(pb)
     end
-    print("\r====Final: $(-best.cost) dollars              \n")
+    verbose && print("\r====Final: $(-best.cost) dollars              \n")
     expandWindows!(pb,best)
     benchmark && return (best,benchData)
     return best
